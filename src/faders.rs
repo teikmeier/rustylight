fd9ccf6d933140eeca32bedd6358400cd4211169
Tobias Eikmeier <tobias_eikmeier@web.de>
2022-06-20 17:15:03 +0100

    feat: add delay and curve stretching

--- src/faders.rs
+++ src/faders.rs
@@ -2,6 +2,7 @@ use serde_yaml::Value;
 use serde_yaml::Mapping;
 use std::time::Instant;
 use std::f64::consts::PI;
+use std::fmt;
 
 pub struct Fader {
     fader_type: FaderType,
@@ -11,29 +12,44 @@ pub struct Fader {
     movement: Option<Movement>
 }
 
+#[derive(Debug)]
 pub enum FaderType {
     Default,
     Midi,
 }
 
+impl fmt::Display for FaderType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
 pub struct Movement {
-    center: Option<u8>,
     delay_percentage: Option<u64>,
     delay_ms: Option<u64>,
     duration_percentage: Option<u64>,
     duration_ms: Option<u64>,
     max: u8,
     min: u8,
+    curve_max: Option<i64>,
+    curve_min: Option<i64>,
     repetition: u8,
     reverse: bool,
     shape: Shape,
 }
 
+#[derive(Debug)]
 pub enum Shape {
     Saw,
     Sine,
     Square,
     Triangle,
+}
+
+impl fmt::Display for Shape {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
 
 impl Fader {
@@ -43,7 +59,7 @@ impl Fader {
 
     pub fn update_state(&mut self, selected_tempo: u8, start_time: Instant) {
         if let Some(movement) = &self.movement {
-            self.current_value = calculate_movement(movement, selected_tempo, start_time, self.current_value);
+            self.current_value = calculate_movement(movement, selected_tempo, start_time);
         } else {
             self.current_value = self.value;
         }
@@ -87,13 +103,14 @@ pub fn fader_from_mapping(channel: &Value, properties: &Value) -> Option<Fader> 
 
 fn movement_from_mapping(movement_input: &Mapping) -> Movement {
     let mut movement = Movement {
-        center: None,
         delay_percentage: None,
         delay_ms: None,
         duration_percentage: None,
         duration_ms: None,
         max: 255,
         min: 0,
+        curve_max: None,
+        curve_min: None,
         repetition: 0,
         reverse: false,
         shape: Shape::Sine,
@@ -103,6 +120,18 @@ fn movement_from_mapping(movement_input: &Mapping) -> Movement {
             movement.max = value.as_u64().unwrap() as u8;
         } else if key.is_string() && key.as_str().unwrap().eq("min") && value.is_number() {
             movement.min = value.as_u64().unwrap() as u8;
+        } else if key.is_string() && key.as_str().unwrap().eq("max_percentage") && value.is_number() {
+            movement.max = ((value.as_f64().unwrap() / 100.0) * 255.0) as u8;
+        } else if key.is_string() && key.as_str().unwrap().eq("min_percentage") && value.is_number() {
+            movement.min = ((value.as_f64().unwrap() / 100.0) * 255.0) as u8;
+        } else if key.is_string() && key.as_str().unwrap().eq("curve_max") && value.is_number() {
+            movement.curve_max = value.as_i64();
+        } else if key.is_string() && key.as_str().unwrap().eq("curve_min") && value.is_number() {
+            movement.curve_min = value.as_i64();
+        } else if key.is_string() && key.as_str().unwrap().eq("curve_max_percentage") && value.is_number() {
+            movement.curve_max = Some(((value.as_f64().unwrap() / 100.0) * 255.0) as i64);
+        } else if key.is_string() && key.as_str().unwrap().eq("curve_min_percentage") && value.is_number() {
+            movement.curve_min = Some(((value.as_f64().unwrap() / 100.0) * 255.0) as i64);
         } else if key.is_string() && key.as_str().unwrap().eq("repetition") && value.is_number() {
             movement.repetition = value.as_u64().unwrap() as u8;
         } else if key.is_string() && key.as_str().unwrap().eq("reverse") && value.is_bool() {
@@ -116,35 +145,54 @@ fn movement_from_mapping(movement_input: &Mapping) -> Movement {
                 "triangle" => movement.shape = Shape::Triangle,
                 _ => movement.shape = Shape::Sine,
             }
-        } else if key.is_string() && key.as_str().unwrap().eq("center") && value.is_number() {
-            movement.center = Some(value.as_u64().unwrap() as u8);
         } else if key.is_string() && key.as_str().unwrap().eq("delay_percentage") && value.is_number() {
-            movement.delay_percentage = Some(value.as_u64().unwrap() as u64);
+            movement.delay_percentage = Some(value.as_u64().unwrap());
         } else if key.is_string() && key.as_str().unwrap().eq("delay_ms") && value.is_number() {
-            movement.delay_ms = Some(value.as_u64().unwrap() as u64);
+            movement.delay_ms = Some(value.as_u64().unwrap());
         } else if key.is_string() && key.as_str().unwrap().eq("duration_percentage") && value.is_number() {
-            movement.duration_percentage = Some(value.as_u64().unwrap() as u64);
+            movement.duration_percentage = value.as_u64();
         } else if key.is_string() && key.as_str().unwrap().eq("duration_ms") && value.is_number() {
-            movement.duration_ms = Some(value.as_u64().unwrap() as u64);
+            movement.duration_ms = value.as_u64();
         }
     }
     // Ensure max is bigger than min
     if movement.max < movement.min {
         movement.min = movement.max;
     }
+    // Ensure curve max and min are set
+    if movement.curve_max.is_none() {
+        movement.curve_max = Some(movement.max as i64);
+    }
+    if movement.curve_min.is_none() {
+        movement.curve_min = Some(movement.min as i64);
+    }
     // Ensure either percentage or ms is set
     if movement.duration_percentage.is_none() && movement.duration_ms.is_none() {
         movement.duration_percentage = Some(400);
     }
-    if movement.delay_percentage.is_none() || movement.delay_ms.is_none() {
+    if movement.delay_percentage.is_none() && movement.delay_ms.is_none() {
         movement.delay_percentage = Some(0);
     }
+    // println!("");
+    // println!("Movement");
+    // println!("  Shape: {}", movement.shape.to_string());
+    // println!("  Delay: {:?}", movement.delay_percentage.unwrap());
+    // println!("  Max: {}", movement.max);
+    // println!("  Min: {}", movement.min);
+    // println!("  Curve max: {}", movement.curve_max.unwrap());
+    // println!("  Curve min: {}", movement.curve_min.unwrap());
+    // println!("  Reverse: {:?}", movement.reverse);
+    // println!("  Repetitions: {:?}", movement.repetition);
+    // println!("  Duration: {:?}", movement.duration_percentage.unwrap());
+    // println!("");
     movement
 }
 
-fn calculate_movement(movement: &Movement, beats_per_minute: u8, start_time: Instant, current_value: u8) -> u8 {
+fn calculate_movement(movement: &Movement, beats_per_minute: u8, start_time: Instant) -> u8 {
     let max = movement.max as f64;
     let min = movement.min as f64;
+    let curve_max = movement.curve_max.unwrap() as f64;
+    let curve_min = movement.curve_min.unwrap() as f64;
     let beat_duration_ms: f64 = 60000.0 / beats_per_minute as f64;
     let movement_duration_ms: f64 = if let Some(percentage) = movement.duration_percentage {
         beat_duration_ms * (percentage as f64 / 100.0)
@@ -153,33 +201,46 @@ fn calculate_movement(movement: &Movement, beats_per_minute: u8, start_time: Ins
     } else {
         beat_duration_ms * 4.0
     };
-    let elapsed = start_time.elapsed().as_millis() as f64;
-    let current_position = (elapsed % movement_duration_ms) / movement_duration_ms;
+    let delay_ms = if let Some(percentage) = movement.delay_percentage {
+        beat_duration_ms * (percentage as f64 / 100.0)
+    } else if let Some(ms) = movement.delay_ms {
+        ms as f64
+    } else {
+        0.0
+    };
+    let elapsed_ms = start_time.elapsed().as_millis() as f64;
+    let current_position = ((elapsed_ms + delay_ms) % movement_duration_ms) / movement_duration_ms;
+    let new_value: f64;
 
     match movement.shape {
         Shape::Sine => {
             let new_unified_value = cos_function(current_position, movement.reverse);
-            (((max - min) * new_unified_value) + min) as u8
+            new_value = ((curve_max - curve_min) * new_unified_value) + curve_min;
         },
         Shape::Square => {
             let new_unified_value = cos_function(current_position, movement.reverse);
             if new_unified_value < 0.5 {
-                min as u8
+                new_value = curve_min;
             } else {
-                max as u8
+                new_value = curve_max;
             }
         },
         Shape::Saw => {
             let new_unified_value = saw_function(current_position, movement.reverse);
-            (((max - min) * new_unified_value) + min) as u8
+            new_value = ((curve_max - curve_min) * new_unified_value) + curve_min;
         },
         Shape::Triangle => {
             let new_unified_value = triangle_function(current_position, movement.reverse);
-            (((max - min) * new_unified_value) + min) as u8
+            new_value = ((curve_max - curve_min) * new_unified_value) + curve_min;
         }
-        _ => {
-            current_value
-        }
+    }
+
+    if new_value >= max {
+        max as u8
+    } else if new_value <= min {
+        min as u8
+    } else {
+        new_value as u8
     }
 }
 
@@ -193,7 +254,7 @@ fn cos_function(mut position: f64, reverse: bool) -> f64 {
     if reverse {
         reverse_operator = 1.0;
     }
-    (-reverse_operator * f64::cos( 2.0 * PI * position ) + 1.0) / 2.0
+    (-1.0 * reverse_operator * f64::cos( 2.0 * PI * position ) + 1.0) / 2.0
 }
 
 fn saw_function(mut position: f64, reverse: bool) -> f64 {
@@ -209,7 +270,6 @@ fn saw_function(mut position: f64, reverse: bool) -> f64 {
 }
 
 fn triangle_function(mut position: f64, reverse: bool) -> f64 {
-    let mut reverse_operator = -1.0;
     if position > 1.0 {
         position = 1.0;
     } else if position < 0.0 {
